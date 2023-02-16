ddb7cf34ecdc3de04b0bcea48c22fdcd8f6b4831
BasilCharway <chawaybasil@gmail.com>
2023-02-16 12:02:02 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
-*main - A C-Programming that print with puts function
-*Return; 0 (success)
+/**
+*main - Entry point
+*
+* Discription: 'A C-Programming that print with puts function'
+*Return: 0 (success)
 */
-int main(void);
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle,");
-return (0);
+*return (0);
 }
 
