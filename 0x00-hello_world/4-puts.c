37fe5492b5103ab19049cf724f71de1167f8349b
BasilCharway <chawaybasil@gmail.com>
2023-02-16 12:36:24 -0800

    pits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 *main - Entry point
 *
 * Discription: 'A C-Programming that print with puts function'
-*Return 0 (success);
+*Return: Always  0 (success)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle,");
-*return (0);
+return (0);
 }
 
