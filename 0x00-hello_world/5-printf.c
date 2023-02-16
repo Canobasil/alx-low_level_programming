d8fd07a60bb0143db0fb18e16233e15c53ce7d96
BasilCharway <chawaybasil@gmail.com>
2023-02-16 14:01:35 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#intclude "<stdio.h>"
+/**
+* main - Entry Point
+* Discription: C Program using printf
+*Return: 0 Always (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
