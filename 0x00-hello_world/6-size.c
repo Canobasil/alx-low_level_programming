e2815f235c42a82af15234fb9c21961d10c817ea
BasilCharway <chawaybasil@gmail.com>
2023-02-17 02:36:04 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,21 @@
  */
 int main(void)
 {
-printf(Size of a char : 1 byte(s)
-		Size of an int : 4 byte(s)
-		Size of a long int : 4 byte(s)
-		Size of a long long int : 8 byte(s)
-		Size of a float : 4 byte(s);
-	return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of a char %lu byte(s)\n", (unsigned long
+sizeof(a)));
+("size of an int %lu byte(s)\n", (unsigned long
+sizeof(b)));
+("size of an long int %lu byte(s)\n", (unsigned long
+sizeof(c)));
+("size of an long long int %lu byte(s)\n", (unsigned long
+sizeof(d)));
+("size of an float %lu byte(s)\n", (unsigned long
+sizeof(f)));
+return (0)
+	;
 }
