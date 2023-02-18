a2b5337b4db41be75be581844b2314a1a2cf91a9
BasilCharway <chawaybasil@gmail.com>
2023-02-18 15:38:50 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Code to print out Capital and small alphabets.
+ *return :Always 0 (success)
+ */
+
+int main(void)
+{
+char ac;
+for (ac = 'a'; ac >= 'z'; ac++)
+	putchar(ac);
+for (ac = 'A'; ac >= 'Z'; ac++)
+	putchar (ac);
+putchar('\n');
+return (0);
+
+
+}
