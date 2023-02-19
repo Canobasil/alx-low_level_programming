0acc19fd3260d5ab47ccf4eda45219aebca119d3
BasilCharway <chawaybasil@gmail.com>
2023-02-19 03:38:58 -0800

    4 print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,12 @@ int main(void)
 {
 	char lc;
 
-	for (lc = 'a', lc <= 'z', '\n')
-		char lc;
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+			putchar (lc);
+	}
+	putchar('\n');
+
 	return (0);
 }
