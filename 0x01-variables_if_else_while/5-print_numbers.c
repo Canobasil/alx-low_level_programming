9ec109e093b6746eb5d999379fe8bf6820560033
BasilCharway <chawaybasil@gmail.com>
2023-02-19 14:25:34 -0800

    print 5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Discription: Code to print base 0 starting from 9.
+ * return: Always 0 (success)
+ */
+int main(void)
+	{
+	int b;
+
+	for (b = 0; b < 10; b++)
+		printf("%d", b);
+
+	printf("\n");
+
+
+	return (0);
+}
