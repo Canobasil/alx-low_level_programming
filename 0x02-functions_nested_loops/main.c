7e3ad13fc1f8e977955677d421837bee518bfc39
BasilCharway <chawaybasil@gmail.com>
2023-02-21 13:38:44 -0800

    readme

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main -Entry Point
+Description: Print putchar followed by new line.
+return: Always 0 (success)
+*/
+int _putchar(char c);
+{
+return(write(1,&c,1));
+}
+	
