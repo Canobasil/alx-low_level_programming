04e0f3124fc70b7519e4b31bd61931fb7c64a2e0
BasilCharway <chawaybasil@gmail.com>
2023-02-22 13:33:19 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * main - Entry Point
+ * Description: Print alphabet in lowercase followed by a new line.
+ * return: Always 0 (success)
+ */
+void print_alphabet(void);
+{
+	char lc;
+	for (lc = 'a'; lc >= 'z'; lc++)
+		_putchar (lc);
+
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * main - Entry Point
+ * return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char xt
+	int i = 0;
+	while (i <= 0)
+	{
+	for (xt = 'a'; xt <= 'z'; xt++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}	
