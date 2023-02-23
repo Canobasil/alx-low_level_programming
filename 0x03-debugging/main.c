538cf9467bd3fbcf8544168c8c5386328c9577a5
BasilCharway <chawaybasil@gmail.com>
2023-02-23 05:46:24 -0800

    0task

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Test function for positive and negative.
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive _or_negative(i);
+
+	return (0);
+}
+
