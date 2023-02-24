86219207c691c4ea0c3f13684e870f65eed7ef09
BasilCharway <chawaybasil@gmail.com>
2023-02-24 13:39:05 -0800

    Is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,17 @@
-##include "main.h"
+#include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * DEscription: The character should be checked
- * return: 1 if the character is upper 0 otherwise.
+ *_isupper - uppercase letters
+ *@c: char to check
  *
+ *Return: 0 or 1
  */
+
 int _isupper(int c)
 {
-	
 	if (c >= 'A' && c <= 'Z')
-	{
 	return (1);
-	}
-	{
 	else
-	}
-	{
 	return (0);
-	}
-}
+}	
 
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mul - multiply 2 int
+ * _mul - multiply 2 int
  * @a - first int
  * @b - second int
  * return: Addition of multiplication
  */
-int mul(int a, int b)
+int _mul(int a, int b)
 {
 	return (a * b);
 }
