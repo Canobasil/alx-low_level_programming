6c3c9faa2974ff87eb19faf976b0dbf0394476cb
BasilCharway <chawaybasil@gmail.com>
2023-02-27 13:35:16 -0800

    return lenght

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * reset - reset n to 98
  * Description: A program that take int parameter and update value point to 98
  * Return 0
@@ -8,5 +7,5 @@
  */
 void reset_to_98(int *n);
 {
-	*n = 98;
+	int *n = 98;
 }
