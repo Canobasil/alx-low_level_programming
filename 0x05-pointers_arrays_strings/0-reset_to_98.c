5a4a90ad4062e34d843099b7c20f999e4bdf0673
BasilCharway <chawaybasil@gmail.com>
2023-02-27 12:28:39 -0800

    First Array Task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *
+ * reset - reset n to 98
+ * Description: A program that take int parameter and update value point to 98
+ * Return 0
+ *
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
