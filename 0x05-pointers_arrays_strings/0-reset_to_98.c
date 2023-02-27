fd750ac7c765e74da3a1c306cb1c6d8de9dac965
BasilCharway <chawaybasil@gmail.com>
2023-02-27 14:45:10 -0800

    switching pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 /**
  * reset - reset n to 98
  * Description: A program that take int parameter and update value point to 98
- * Return 0
- *
+ * @n: input
+ *Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int*n);
 {
-	int *n = 98;
+	*n=98;
 }
