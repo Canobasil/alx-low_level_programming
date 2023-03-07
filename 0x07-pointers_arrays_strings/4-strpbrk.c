7380c3137dbaeba0b8972495d7089dc571147244
BasilCharway <chawaybasil@gmail.com>
2023-03-07 12:08:32 -0800

    mains

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - locate the first occurance in the string
+ * *s: string pointer byte
+ * *accept: accepted byte
+ *return: "%p" to byte in "s" if one of the bytes accepts else Null
+ */
+char *_strpbrk(char *s, char *accept);
+{
+
+	char *s;
+	char *accept;
+
+
+}
