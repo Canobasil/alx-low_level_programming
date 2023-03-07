4dbb2dc6a107a5144ea39416002f1524ecc26589
BasilCharway <chawaybasil@gmail.com>
2023-03-07 15:02:39 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - Finds the first occurrence of the string
+ *@haystack: input
+ *@needle: input
+ * return: to beginning else null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for(; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *d = needle;
+
+		while(*l == *p && *p != '\0')
+		{
+			l++;
+			d++;
+		}
+		if (*d == '\0')
+			return (haystack);
+	}
+	return (0);
+}
