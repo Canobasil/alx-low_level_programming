236e168548946e7b94e10d78aa220883277d1244
BasilCharway <chawaybasil@gmail.com>
2023-03-08 14:28:42 -0800

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _rev_recursions - a program that reverse, recurtions
+ * @s: string input
+ * return: 0 Always (success)
+ */
+void _print_rev_recursion(char *s)	
+{
+	char *s;
+
+	{
+	reverse(s + 1);
+put("%c" * s);
+	('\n')
+	}
+	return (0);
+}
