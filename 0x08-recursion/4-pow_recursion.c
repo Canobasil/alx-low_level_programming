5f042b11dd297160ec6c9825c4f88c3d33b0f5b7
BasilCharway <chawaybasil@gmail.com>
2023-03-09 05:56:48 -0800

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,15 +6,12 @@
  * @y: power  int variable
  * return: 0 else if y is less than 0 return -1
  */
-int _pow_recursion(int, int y)
+int _pow_recursion(int x, int y)
 {
-	int x;
-	int y;
-	{
+
 		if (y == 0)
 			return (1);
 		if (y < 0)
 			return (-1);
 		return (x * _pow_recursion(x, y - 1));
-	}
 }
