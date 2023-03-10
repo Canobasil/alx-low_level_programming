a6c34985ccfa2bfed951c89b6f53afc4ae381a14
BasilCharway <chawaybasil@gmail.com>
2023-03-10 15:00:23 -0800

    Argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main - A program to print it's name
+ *@argv: argument vector
+ *@argc: argument count
+ */
+int main(char argv, int argc)
+{
+	printf("argv\n");
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+readme for Arguments in C
